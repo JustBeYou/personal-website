5ad4a6396ad8d8283fd16bdc67d0a0ba19fba24b
JustBeYou <mihailferaru2000@gmail.com>
2020-03-02 00:57:07 -0800

    Little save

--- main-layout.js
+++ main-layout.js
@@ -3,7 +3,7 @@
 const asideExpandedWidth  = "200px";
 const asideCollapsedWidth = "0px";
 const asideSelector = "div.main-layout aside";
-let asideMenuState = "expanded"; // collapsed, expanded or top
+let asideMenuState = "collapsed"; // collapsed, expanded or top
 
 function resizeAside(size) {
     const asideElement = document.querySelector(asideSelector);
@@ -21,42 +21,13 @@ function switchAsideState() {
 }
 
 function handleAsideResize() {
-    if (window.innerWidth <= 768) return ;
-    
     const asideElement = document.querySelector(asideSelector);
     switchAsideState();
-}
-
-function moveAsideToTop() {
-    const asideElement = document.querySelector(asideSelector);
-    asideElement.style.width = "100%";
-    asideMenuState = "top";
-}
-
-function applyMobileView() {
-    moveAsideToTop();
-}
-
-function applyWebView() {
-    switchAsideState();
-}
-
-function handleWindowResize(mobileViewEvent) {
-    if (mobileViewEvent.matches) {
-        applyMobileView();
-    } else {
-        applyWebView();
-    }
 }
 
 function onloadHandler() {
     const activator = document.querySelector(".activate-aside");
     activator.addEventListener('click', handleAsideResize);
-    
-    const mobilePortView = window.matchMedia("(max-width: 768px)");
-    mobilePortView.addListener(handleWindowResize);
-    
-    handleWindowResize(mobilePortView);
 }
 
 window.onload = onloadHandler;
