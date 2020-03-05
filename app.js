b0a63c6001e7a3200167e6df8eeb5b1ff942d503
JustBeYou <mihailferaru2000@gmail.com>
2020-03-05 17:31:51 +0200

    var to const

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const process = require('process');
 
-var indexRouter = require('./routes/index');
+const database = require('./database.js');
+const indexRouter = require('./routes/index.js');
 
-var app = express();
+database();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
