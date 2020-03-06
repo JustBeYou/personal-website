b5a182792de2b5bdce58d8ab90464ca906500327
JustBeYou <mihailferaru2000@gmail.com>
2020-03-06 11:05:41 +0200

    Make a default installer

---
+++ install.js
@@ -0,0 +1,41 @@
+const installerConfig = require('./default/installer.json');
+const database = require('./database.js');
+const Page = require('./models/page.js');
+const process = require('process');
+const fs = require('fs');
+
+async function install() {
+    console.warn("If there is any content in the database it will be deleted!");
+    await database();
+    await installWebpages();
+}
+
+async function installWebpages() {
+    console.log("Installing default web pages...");
+    await Page.remove({});
+    
+    for (const page of installerConfig.pages) {
+        const content = fs.readFileSync(page.filePath, 'utf-8');
+        delete page.filePath;
+        
+        const newPage = new Page({
+            ...page,
+            content,
+        });
+            
+        await newPage.save();
+        console.log("Created", page.title);
+    }
+    
+    console.log("Done.");
+}
+
+install()
+    .then(() => {
+        console.log("Installed successfully.");
+        process.exit(0);
+    })
+    .catch((err) => {
+        console.log("Could not install.", err.toString());
+        process.exit(-1);
+    });
