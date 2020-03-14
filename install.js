bc8eebff8f80fe92581d580cf97b8fda091e873e
JustBeYou <mihailferaru2000@gmail.com>
2020-03-14 03:49:03 -0700

    Config.

--- install.js
+++ install.js
@@ -1,20 +1,45 @@
 const installerConfig = require('./default/installer.json');
 const database = require('./database.js');
 const Page = require('./models/page.js');
+const User = require('./models/user.js');
 const process = require('process');
 const fs = require('fs');
 const {ncp} = require('ncp');
 const {exec} = require('child_process');
 
 async function install() {
-    console.warn("If there is any content in the database it will be deleted!");
+    console.warn("If there is any content in the database it could be deleted!");
     await database();
+
+    let name = 'admin', password = 'admin', email = 'admin@example.com';
+    if (process.argv.length < 4) {
+        console.log("Using default creds...");
+    } else {
+        name     = process.argv[1];
+        password = process.argv[2];
+        email    = process.argv[3];
+    }
+
+    try {
+        const newAdmin = new User({name, password, email, admin: true});
+        await newAdmin.save();
+        console.log('Admin:', newAdmin);
+    } catch (err) {
+        console.log('Error: ', err);
+    }
+
     await copyResources();
     await installWebpages();
 }
 
 async function copyResources() {
-    await exec('mkdir public; mkdir public/images; mkdir public/javascripts; mkdir public/stylesheets; mkdir views/parts; mkdir views/pages;');
+    await exec(`mkdir -p public/admin &&
+                mkdir -p public/images &&
+                mkdir -p public/javascripts && 
+                mkdir -p public/stylesheets &&
+                mkdir -p views/parts &&
+                mkdir -p views/pages`);
+    await new Promise(r => setTimeout(r, 1500));
     await ncp('default/resources/images', 'public/images');
     await ncp('default/resources/javascripts', 'public/javascripts');
     await ncp('default/resources/stylesheets', 'public/stylesheets');
